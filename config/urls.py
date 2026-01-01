915c7b5b68cafcd53c05d00c8b358696d3c978b4
Omar Hesham <omarhesham1192019@gmail.com>
2026-01-01 21:45:25 +0200

    Add course listing template and configure project-wide URL routing.

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.shortcuts import render
+from django.conf import settings
+from django.conf.urls.static import static
 
 def home(request):
     return render(request, 'home.html')
@@ -9,6 +11,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name='home'),
     path('accounts/', include('accounts.urls')),
-    path('accounts/', include('django.contrib.auth.urls')), # Built-in auth (login, logout, password reset)
+    path('accounts/', include('django.contrib.auth.urls')), # Built-in auth
     path('courses/', include('courses.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
