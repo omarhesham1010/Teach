86fdae5f23ce92078deac8fd061c870de3564849
Omar Hesham <omarhesham1192019@gmail.com>
2026-01-29 03:37:54 +0200

    Add Initial Front End

---
+++ myproject/teach/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.login_view, name='login'), # Default to login
+    path('instructor/', views.instructor, name='instructor'),
+    path('courses/', views.courses, name='courses'),
+    path('course-details/', views.course_details, name='course_details'),
+    path('course-content/', views.course_content, name='course_content'),
+    path('assignment/', views.assignment, name='assignment'),
+    path('enrollment/', views.enrollment, name='enrollment'),
+    path('login/', views.login_view, name='login'),
+    path('signup/', views.signup, name='signup'),
+    path('quiz/', views.quiz, name='quiz'),
+    path('quiz-submit/', views.quiz_submit, name='quiz_submit'),
+    path('submission-success/', views.submission_success, name='submission_success'),
+]
--- myproject/teach/views.py
+++ myproject/teach/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
 
-# Create your views here.
+def instructor(request):
+    return render(request, 'teach/instructor.html')
+
+def courses(request):
+    return render(request, 'teach/courses.html')
+
+def course_details(request):
+    return render(request, 'teach/coursedetails.html')
+
+def course_content(request):
+    return render(request, 'teach/coursecontent.html')
+
+def assignment(request):
+    return render(request, 'teach/assignment.html')
+
+def enrollment(request):
+    return render(request, 'teach/enrollement.html')
+
+def login_view(request):
+    return render(request, 'teach/login.html')
+
+def signup(request):
+    return render(request, 'teach/signup.html')
+
+def quiz(request):
+    return render(request, 'teach/quiz.html')
+
+def quiz_submit(request):
+    return render(request, 'teach/quizsummit.html')
+
+def submission_success(request):
+    return render(request, 'teach/submissionsuccess.html')
